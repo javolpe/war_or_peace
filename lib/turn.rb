018fa886333a4587345126228cfd3cc3a1ecd180
Jake Volpe <volpe.jacob@gmail.com>
2020-12-02 19:09:02 -0700

    Add type and type_test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,1 +1,22 @@
 class Turn
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def type
+    if self.player1.deck.cards[0].rank != self.player2.deck.cards[0].rank
+      :basic
+    elsif (self.player1.deck.cards[0].rank == self.player2.deck.cards[0].rank) && self.player1.deck.cards[2].rank == self.player2.deck.cards[2].rank
+      :mutually_assured_destruction
+    elsif self.player1.deck.cards[0].rank == self.player2.deck.cards[0].rank
+      :war
+    end
+  end
+
+end
