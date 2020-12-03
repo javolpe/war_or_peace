2ee416af6bbbe5ab10b2c0392a8fd1d4b67653ef
Jake Volpe <volpe.jacob@gmail.com>
2020-12-02 20:45:06 -0700

    Finish pile_cards and test_pile variants

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ class Turn
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @spoils_of_war = []
+    @spoils_of_war = Array.new
   end
 
   def type
@@ -32,5 +32,26 @@ class Turn
       "No Winner"
     end
   end
+
+  def pile_cards
+    if self.type == :basic
+      @spoils_of_war << player1.deck.cards[0]
+      @spoils_of_war << player2.deck.cards[0]
+    elsif self.type == :war
+      @spoils_of_war << player1.deck.cards[0]
+      @spoils_of_war << player1.deck.cards[1]
+      @spoils_of_war << player1.deck.cards[2]
+#tried this and it didn't work  @spoils_of_war << player1.deck.cards[0..2]
+      @spoils_of_war << player2.deck.cards[0]
+      @spoils_of_war << player2.deck.cards[1]
+      @spoils_of_war << player2.deck.cards[2]
+    else
+      3.times do
+        self.player1.deck.remove_card
+        self.player2.deck.remove_card
+      end
+    end
+  end
+
 
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -79,8 +79,16 @@ class TurnTest < Minitest::Test
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
+
+#  card3 = Card.new(:heart, '9', 11)
+#switch out two instances of card3 to test :war
+
     card4 = Card.new(:diamond, 'Jack', 11)
     card5 = Card.new(:heart, '8', 8)
+
+#    card5 = Card.new(:heart, '8', 12)
+#switch out  card3&card5 to test :mutually_assured_destruction
+
     card6 = Card.new(:diamond, 'Queen', 12)
     card7 = Card.new(:heart, '3', 3)
     card8 = Card.new(:diamond, '2', 2)
@@ -92,7 +100,20 @@ class TurnTest < Minitest::Test
     player2 = Player.new('Aurora', deck2)
     turn = Turn.new(player1, player2)
 
-    assert_equal turn.pile_cards, turn.spoils_of_war = [card1, card3]
+    turn.pile_cards
+    wanted_array = [card1, card3]
+
+#    wanted_array = [card1, card2, card5, card3, card4, card6]
+#us3 this wanted array for testing :war
+
+#    wanted_array = [card8]
+#us this wanted array for testing :mutually_assured_destruction
+
+
+   assert_equal turn.spoils_of_war, wanted_array
+
+#    assert_equal turn.player1.deck.cards, wanted_array
+#use above assertion for testing :mutually_assured_destruction
 
   end
 
